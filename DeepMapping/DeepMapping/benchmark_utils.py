a016a45178fa0305e8dcb796c8b16b1688932e59
Lixi Zhou <zhoulx2013@gmail.com>
2023-09-01 20:53:32 -0700

    update link

--- DeepMapping/DeepMapping/benchmark_utils.py
+++ DeepMapping/DeepMapping/benchmark_utils.py
@@ -1,4 +1,4 @@
-from DeepMapping import dgpe_compression, byte_dictionary_compression, delta_compression, lzo_compression, zstd_compression, uncompress, rle_compression, deepmapping
+from DeepMapping import dgpe_compression, byte_dictionary_compression, delta_compression, lzo_compression, zstd_compression, uncompress, rle_compression, deepmapping, hashtable, hashtable_with_compression
 
 
 def benchmark_handler(benchmark, bench_type='single'):
@@ -19,10 +19,13 @@ def benchmark_handler(benchmark, bench_type='single'):
             return zstd_compression.measure_latency
         elif benchmark == "rle":
             return rle_compression.measure_latency
+        elif benchmark == 'hashtable':
+            return hashtable.measure_latency
+        elif benchmark == 'hashtable_with_compression':
+            return hashtable_with_compression.measure_latency
         elif benchmark == "deepmapping":
             return deepmapping.measure_latency_any
         else:
             raise ValueError("NON-EXIST benchmark")
     else:
         raise ValueError("Non supported bench_type")
-        
